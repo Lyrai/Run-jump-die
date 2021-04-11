6f130f83d2bd109fd88d9319b8ae6004364a9191
Mixail <mixail-12-r@mail.ru>
2021-04-11 21:59:12 +0000

    No Comments

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private Dropdown _dropdown;
+    private void Start()
+    {
+        
+    }
+    public void SetСontrols() 
+    {
+        PlayerPrefs.SetInt("Controls",_dropdown.value);
+        Debug.Log(_dropdown.value);
+    }
     public void Play()
     {
         SceneManager.LoadScene(1);
@@ -14,4 +24,5 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
 }
