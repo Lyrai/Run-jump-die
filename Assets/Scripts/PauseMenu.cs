6ad37972e4f2349eb324acb49d7d141c50d6ccea
Dmitry <x355x02c@live.com>
2021-04-12 07:27:09 +0000

    Добавлено меню паузы, переименована сцена

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+    private bool _isOpen;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            _isOpen = !_isOpen;
+            Time.timeScale = _isOpen ? 0 : 1;
+            pauseMenu.SetActive(_isOpen);
+        }
+    }
+}
